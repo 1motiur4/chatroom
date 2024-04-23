4442fcc8449ea38372035a66863d63339bebbde9
Motiur Muhith <1motiur4@gmail.com>
2024-04-23 16:14:14 -0400

    initial chatbox started

--- app/javascript/controllers/chatroom_controller.js
+++ app/javascript/controllers/chatroom_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["input", "chatbox"];
+
   connect() {
-    console.log("hi")
+    console.log("hi");
+  }
+
+  send() {
+    let msgText = this.inputTarget.value;
+    this.chatboxTarget.innerHTML += `<div>${msgText}</div>`
+    this.inputTarget.value = ""
   }
 }
